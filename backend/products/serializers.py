257b1c6406f448ddca248e17003fd6fe386dd287
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2023-02-10 16:46:58 +0530

    Custom Validation

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,18 +1,43 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer): #responsible for converting objects into data types understandable by javascript and front-end frameworks
     my_discount = serializers.SerializerMethodField(read_only=True)
+    # url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk'
+    )
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
+            'url',
+            'email',
+            'pk',
             'title',
             'content',
             'price',
             'sale_price',
             'my_discount'
         ]
+    def create(self, validated_data):
+        # return Product.objects.create(**validated_data)
+        email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        return obj
+
+    def update(self, instance, validated_data):
+        
+        instance.title = validated_data.get('title')
+        return instance
+
+    def get_url(self,obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-detail", kwargs={"pk" : obj.pk}, request=request)
     def get_my_discount(self,obj):
         if not hasattr(obj, 'id'):
             return None
